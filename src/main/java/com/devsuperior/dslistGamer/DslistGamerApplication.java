06fe93e167e82fb89e29e664a66449ae2054d7a2
Uillian Leal <willianleal52@hotmail.com>
2023-05-11 11:39:08 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/dslistGamer/DslistGamerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistGamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslistGamer/DslistGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
